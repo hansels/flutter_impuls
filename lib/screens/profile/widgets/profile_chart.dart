541c67c4d26823fc10bc03767d8737ffb2471cd2
hansels <hansel@susanto.me>
2022-06-18 21:37:52 +0700

    add graph implementation

---
+++ lib/screens/profile/widgets/profile_chart.dart
@@ -0,0 +1,213 @@
+import 'package:fl_chart/fl_chart.dart';
+import 'package:flutter/material.dart';
+
+class ProfileChart extends StatefulWidget {
+  ProfileChart({Key key}) : super(key: key);
+
+  @override
+  State<ProfileChart> createState() => _ProfileChartState();
+}
+
+class _ProfileChartState extends State<ProfileChart> {
+  final Color barColor = const Color(0xff53fdd7);
+  final double width = 21;
+
+  List<BarChartGroupData> rawBarGroups;
+  List<BarChartGroupData> showingBarGroups;
+
+  int touchedGroupIndex = -1;
+
+  @override
+  void initState() {
+    super.initState();
+    final barGroup1 = makeGroupData(0, 5);
+    final barGroup2 = makeGroupData(1, 16);
+    final barGroup3 = makeGroupData(2, 18);
+    final barGroup4 = makeGroupData(3, 20);
+    final barGroup5 = makeGroupData(4, 17);
+    final barGroup6 = makeGroupData(5, 19);
+    final barGroup7 = makeGroupData(6, 10);
+
+    final items = [
+      barGroup1,
+      barGroup2,
+      barGroup3,
+      barGroup4,
+      barGroup5,
+      barGroup6,
+      barGroup7,
+    ];
+
+    rawBarGroups = items;
+
+    showingBarGroups = rawBarGroups;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return AspectRatio(
+      aspectRatio: 1.2,
+      child: Card(
+        elevation: 0,
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)),
+        color: const Color(0xff2c4260),
+        child: Padding(
+          padding: const EdgeInsets.all(16),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            mainAxisAlignment: MainAxisAlignment.start,
+            mainAxisSize: MainAxisSize.max,
+            children: <Widget>[
+              Row(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                mainAxisSize: MainAxisSize.min,
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: <Widget>[
+                  makeTransactionsIcon(),
+                  const SizedBox(width: 16),
+                  const Text(
+                    'Penghematan Bulan Ini',
+                    style: TextStyle(color: Colors.white, fontSize: 26),
+                  ),
+                ],
+              ),
+              const SizedBox(height: 38),
+              Expanded(
+                child: BarChart(
+                  BarChartData(
+                    titlesData: FlTitlesData(
+                      show: true,
+                      rightTitles: AxisTitles(
+                        sideTitles: SideTitles(showTitles: false),
+                      ),
+                      topTitles: AxisTitles(
+                        sideTitles: SideTitles(showTitles: false),
+                      ),
+                      bottomTitles: AxisTitles(
+                        sideTitles: SideTitles(
+                          showTitles: true,
+                          getTitlesWidget: bottomTitles,
+                          reservedSize: 42,
+                        ),
+                      ),
+                      leftTitles: AxisTitles(
+                        sideTitles: SideTitles(
+                          showTitles: true,
+                          reservedSize: 28,
+                          interval: 1,
+                          getTitlesWidget: leftTitles,
+                        ),
+                      ),
+                    ),
+                    borderData: FlBorderData(
+                      show: false,
+                    ),
+                    barGroups: showingBarGroups,
+                    gridData: FlGridData(show: false),
+                    maxY: 20,
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget leftTitles(double value, TitleMeta meta) {
+    const style = TextStyle(
+      color: Color(0xff7589a2),
+      fontWeight: FontWeight.bold,
+      fontSize: 14,
+    );
+    String text;
+    if (value == 0) {
+      text = '1K';
+    } else if (value == 10) {
+      text = '5K';
+    } else if (value == 19) {
+      text = '10K';
+    } else {
+      return Container();
+    }
+    return SideTitleWidget(
+      axisSide: meta.axisSide,
+      space: 0,
+      child: Text(text, style: style),
+    );
+  }
+
+  Widget bottomTitles(double value, TitleMeta meta) {
+    List<String> titles = ["Mn", "Te", "Wd", "Tu", "Fr", "St", "Su"];
+
+    Widget text = Text(
+      titles[value.toInt()],
+      style: const TextStyle(
+        color: Color(0xff7589a2),
+        fontWeight: FontWeight.bold,
+        fontSize: 14,
+      ),
+    );
+
+    return SideTitleWidget(
+      axisSide: meta.axisSide,
+      space: 16, //margin top
+      child: text,
+    );
+  }
+
+  BarChartGroupData makeGroupData(int x, double y1) {
+    return BarChartGroupData(barsSpace: 4, x: x, barRods: [
+      BarChartRodData(toY: y1, color: barColor, width: width),
+    ]);
+  }
+
+  Widget makeTransactionsIcon() {
+    const width = 4.5;
+    const space = 3.5;
+    return Row(
+      mainAxisSize: MainAxisSize.min,
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: <Widget>[
+        Container(
+          width: width,
+          height: 10,
+          color: Colors.white.withOpacity(0.4),
+        ),
+        const SizedBox(
+          width: space,
+        ),
+        Container(
+          width: width,
+          height: 28,
+          color: Colors.white.withOpacity(0.8),
+        ),
+        const SizedBox(
+          width: space,
+        ),
+        Container(
+          width: width,
+          height: 42,
+          color: Colors.white.withOpacity(1),
+        ),
+        const SizedBox(
+          width: space,
+        ),
+        Container(
+          width: width,
+          height: 28,
+          color: Colors.white.withOpacity(0.8),
+        ),
+        const SizedBox(
+          width: space,
+        ),
+        Container(
+          width: width,
+          height: 10,
+          color: Colors.white.withOpacity(0.4),
+        ),
+      ],
+    );
+  }
+}
--- lib/screens/wishlist/wishlist_screen.dart
+++ lib/screens/wishlist/wishlist_screen.dart
@@ -1,11 +1,14 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_impuls/basics/screens/base_screen.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_impuls/basics/screens/base_screen_with_app_bar.dart';
 import 'package:flutter_impuls/enums/page_name.dart';
 import 'package:flutter_impuls/screens/wishlist/wishlist_page.dart';
+import 'package:flutter_impuls/widgets/custom/custom_text.dart';
 
-class WishlistScreen extends BaseScreen {
+class WishlistScreen extends BaseScreenWithAppBar {
   WishlistScreen()
       : super(
+          "Wishlist",
           PageName.Wishlist,
           scrollable: false,
           padding: EdgeInsets.zero,
@@ -19,5 +22,17 @@ class WishlistScreen extends BaseScreen {
   @override
   Widget drawer(BuildContext context) {
     return null;
+  }
+
+  @override
+  Widget appBar(BuildContext context) {
+    return AppBar(
+      title: const CustomText("Wishlist", fontSize: 20),
+      centerTitle: true,
+      systemOverlayStyle: SystemUiOverlayStyle.dark,
+      backgroundColor: Colors.transparent,
+      elevation: 0,
+      iconTheme: const IconThemeData(color: Colors.black),
+    );
   }
 }
